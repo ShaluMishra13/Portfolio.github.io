83a8b1a07b58b09f5c298abdd1e88e4fa1bcc9ad
ShaluMishra13 <shalu132001@gmail.com>
2022-10-16 19:55:43 +0530

    added my portfolio Website

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+});
